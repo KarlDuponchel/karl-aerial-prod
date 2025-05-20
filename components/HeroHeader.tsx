e768fb63a4d8693e7efce95aceb6ad3381b080b6
KarlDup <karl.d@snt2.fr>
2025-05-20 16:33:26 +0200

    lenis scroll to add

--- components/HeroHeader.tsx
+++ components/HeroHeader.tsx
@@ -16,7 +16,7 @@ export default function HeroHeader() {
             <h1 className="font-unbounded uppercase text-primary text-4xl font-bold">Karl Duponchel - Télépilote Professionnel</h1>
             <hr className="w-1/3 border-primary" />
             <h1 className="font-unbounded uppercase text-primary text-2xl font-bold italic">Prenez de la hauteur sur vos projets</h1>
-            <h1 className="font-unbounded uppercase text-primary text-2xl font-bold italic">Contact à faire</h1>
+            <h1 className="font-unbounded uppercase text-primary text-2xl font-bold italic">Contact à faire + lenis scroll</h1>
         </div>
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40 z-0" />
     </div>
